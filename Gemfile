5064112d2b11da2276270dac1b309dd602287262
Timur Vafin <timur.vafin@flatstack.com>
2013-08-16 20:32:00 +0400

    Add slim, scss

--- Gemfile
+++ Gemfile
@@ -1,4 +1,9 @@
 source 'http://rubygems.org'
 
+# ruby '1.9.3'
+
 gem 'middleman', '~> 3.0.14'
 gem 'middleman-deploy'
+
+gem 'slim', '~> 1.3.9'
+
--- config.rb
+++ config.rb
@@ -1,10 +1,8 @@
+require 'slim'
+
 activate :deploy do |deploy|
   deploy.method = :git
 end
-
-# set :css_dir, 'stylesheets'
-# set :js_dir, 'javascripts'
-# set :images_dir, 'images'
 
 configure :build do
   activate :minify_css
